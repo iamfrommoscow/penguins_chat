0a27b6a8f3d46d22863a085ec653586c83a1ef52
Vitaly <79vitaly79@gmail.com>
2019-04-27 12:15:44 +0300

    Added database

---
+++ db/initDB.go
@@ -0,0 +1,63 @@
+package db
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/jackc/pgx"
+	"io/ioutil"
+	"os"
+)
+
+var connection *pgx.ConnPool = nil
+
+var connectionConfig pgx.ConnConfig
+var connectionPoolConfig = pgx.ConnPoolConfig{
+	MaxConnections: 8,
+}
+
+func initConfig() error {
+
+	dir, err := os.Getwd()
+	if err != nil {
+		fmt.Println(err)
+		return err
+
+	}
+	file, err := os.Open(dir + "/configs/database.json")
+	if err != nil {
+		fmt.Println(err)
+	}
+	body, _ := ioutil.ReadAll(file)
+	err = json.Unmarshal(body, &connectionConfig)
+	if err != nil {
+		fmt.Println(err)
+		return err
+	}
+
+	connectionPoolConfig.ConnConfig = connectionConfig
+
+	return nil
+}
+
+func Connect() error {
+	if connection != nil {
+		return nil
+	}
+	err := initConfig()
+	if err != nil {
+		return err
+	}
+	connection, err = pgx.NewConnPool(connectionPoolConfig)
+	if err != nil {
+		fmt.Println(err)
+		return err
+	}
+	return nil
+}
+
+func Disconnect() {
+	if connection != nil {
+		connection.Close()
+		connection = nil
+	}
+}
--- go.mod
+++ go.mod
@@ -3,6 +3,12 @@ module chat
 go 1.12
 
 require (
+	github.com/cockroachdb/apd v1.1.0 // indirect
 	github.com/golang/protobuf v1.3.1
+	github.com/jackc/fake v0.0.0-20150926172116-812a484cc733 // indirect
+	github.com/jackc/pgx v3.3.0+incompatible
+	github.com/pkg/errors v0.8.1 // indirect
+	github.com/satori/go.uuid v1.2.0 // indirect
+	github.com/shopspring/decimal v0.0.0-20180709203117-cd690d0c9e24 // indirect
 	google.golang.org/grpc v1.20.1
 )
